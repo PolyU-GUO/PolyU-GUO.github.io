e742a6834a55c0fec5a061888ebefc0f8b4dd66c
Chang GUO <chang.guo@connect.polyu.hk>
2024-05-16 22:42:08 -0400

    try

--- js/function.js
+++ js/function.js
@@ -3,7 +3,7 @@ let $window = $(window),garden;
 $(function () {
     // setup garden
 	let $loveHeart = $("#loveHeart"),
-	    offsetX = $loveHeart.width() / 2,
+	    offsetX = $loveHeart.width() / 2-160,
 			offsetY = $loveHeart.height() / 2 - 55,
 			$garden = $("#garden"),
 			gardenCanvas = $garden[0],
@@ -27,8 +27,8 @@ $(function () {
 // 返回爱心点的坐标
 function getHeartPoint(angle) {
 	var t = angle / Math.PI;
-	var x = 19.5 * (16 * Math.pow(Math.sin(t), 3));
-	var y = - 20 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
+	var x = 19.5/3 * (16 * Math.pow(Math.sin(t), 3));
+	var y = - 20/3 * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t));
 	return new Array(offsetX + x, offsetY + y);
 }
 
@@ -86,6 +86,10 @@ function startHeartAnimation() {
 function timeElapse(date) {
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
+	var years = Math.floor(seconds / (3600 * 24*365));
+	seconds = seconds % (3600 * 24*365);
+	var months = Math.floor(seconds / (3600 * 24*30));
+	seconds = seconds % (3600 * 24*30);
 	var days = Math.floor(seconds / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
@@ -101,7 +105,7 @@ function timeElapse(date) {
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds";
+	var result = "<span class=\"digit\">"+ years + "</span> 年 <span class=\"digit\">"+months + "</span> 月 <span class=\"digit\">"+ days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">";
 	$('#elapseClock').html(result);
 }
 
